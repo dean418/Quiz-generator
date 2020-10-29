c004bbc5fb57128f68fad7a2a702f4f3b62621d0
dean418 <deansingleton1999@gmail.com>
2020-10-29 21:08:06 +0000

    Basic structure

---
+++ index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
+const express = require('express');
+
+const router = require('./routes/router.js');
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(session({
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2,
+        sameSite: true,
+        secure: process.env.IN_PROD
+    }
+}));
+
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log(`server listening on port ${process.env.port || 3000}`)
+});
