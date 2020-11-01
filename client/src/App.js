11b2753bfebcde9eac233055bd69f6cc038b29f6
dean418 <deansingleton1999@gmail.com>
2020-11-01 01:31:42 +0000

    added button to create a room

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+    constructor() {
+        super();
 
-    </div>
-  );
+        this.state = {
+            key: ''
+        }
+    }
+
+    createRoom = () => {
+        fetch('http://localhost:3001/room/create')
+            .then(response => response.json())
+                .then(response => this.setState({key: response.key}));
+    }
+
+    render () {
+        return (
+            <div className = "App" >
+                <Router>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div className="roomBtns">
+                        <button onClick={this.createRoom}>Create a room</button>
+                        <button>Join a room</button>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
