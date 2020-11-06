9269ff94db7dbc5bde5444bc1ec9a8ad32d7ec4f
dean418 <deansingleton1999@gmail.com>
2020-11-06 23:51:52 +0000

    Imported context into lobby to display the room key

--- client/src/lobby/lobby.js
+++ client/src/lobby/lobby.js
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
+
+import Context from '../context';
 import './lobby.css';
 
 const Lobby = () => {
+    const state = useContext(Context);
+
     return (
         <div className='teamContainer'>
-            <h3 className='notice'>You can join this room using the key: <span>12345678</span></h3>
+            <h3 className='notice'>You can join this room using the key: <span>{state.roomKey}</span></h3>
             <h1 className='title'>Contestants</h1>
             <div className='names'>
                 <div className='team'>
